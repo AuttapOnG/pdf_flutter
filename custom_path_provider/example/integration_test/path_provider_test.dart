d8ef0cf05694305b9f09f57db4a1fb2aca9d615c
Auttapong Tura <auttapong.tura@scbtechx.io>
2021-09-27 13:00:12 +0700

    Add example

--- custom_path_provider/example/integration_test/path_provider_test.dart
+++ custom_path_provider/example/integration_test/path_provider_test.dart
@@ -5,7 +5,7 @@
 import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:custom_path_provider/path_provider.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
--- custom_path_provider/example/lib/main.dart
+++ custom_path_provider/example/lib/main.dart
@@ -7,7 +7,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:custom_path_provider/path_provider.dart';
 
 void main() {
   runApp(MyApp());
