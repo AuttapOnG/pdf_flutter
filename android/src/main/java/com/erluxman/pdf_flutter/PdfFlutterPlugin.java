606ef102c74c7763bff44278d6d7d0879b906f3f
Auttapong Tura <auttapong.tura@scbtechx.io>
2021-10-19 16:21:47 +0700

    disable SuppressWarnings

--- android/src/main/java/com/erluxman/pdf_flutter/PdfFlutterPlugin.java
+++ android/src/main/java/com/erluxman/pdf_flutter/PdfFlutterPlugin.java
@@ -1,13 +1,16 @@
 package com.erluxman.pdf_flutter;
 
-import io.flutter.plugin.common.PluginRegistry.Registrar;
+/**
+ * PdfFlutterPlugin
+ */
+public class PdfFlutterPlugin{
 
-/** PdfFlutterPlugin */
-public class PdfFlutterPlugin {
-  public static void registerWith(Registrar registrar) {
-    registrar
-            .platformViewRegistry()
-            .registerViewFactory(
-                    "pdf_flutter_plugin", new PdfFlutterFactory(registrar.messenger()));
-  }
+    @SuppressWarnings("deprecation")
+    public static void registerWith(io.flutter.plugin.common.PluginRegistry.Registrar registrar) {
+        registrar
+                .platformViewRegistry()
+                .registerViewFactory(
+                        "pdf_flutter_plugin", new PdfFlutterFactory(registrar.messenger()));
+
+    }
 }
