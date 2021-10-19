93683a491fd9e6c7d0357b0d56b7da1562e3d896
AuttapOnG <ace.auttapong@gmail.com>
2021-10-19 20:29:15 +0700

    Migration to v2 embedding

--- android/src/main/java/com/erluxman/pdf_flutter/PdfFlutterPlugin.java
+++ android/src/main/java/com/erluxman/pdf_flutter/PdfFlutterPlugin.java
@@ -1,16 +1,22 @@
 package com.erluxman.pdf_flutter;
 
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+
 /**
  * PdfFlutterPlugin
  */
-public class PdfFlutterPlugin{
+public class PdfFlutterPlugin implements FlutterPlugin {
 
-    @SuppressWarnings("deprecation")
-    public static void registerWith(io.flutter.plugin.common.PluginRegistry.Registrar registrar) {
-        registrar
-                .platformViewRegistry()
-                .registerViewFactory(
-                        "pdf_flutter_plugin", new PdfFlutterFactory(registrar.messenger()));
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+        binding.getPlatformViewRegistry().registerViewFactory(
+                "pdf_flutter_plugin", new PdfFlutterFactory(binding.getBinaryMessenger()));
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
 
     }
 }
